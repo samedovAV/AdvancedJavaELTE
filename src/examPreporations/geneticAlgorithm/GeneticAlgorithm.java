ed7be3b950a11e01e56aee073d78e976975c5892
samedovAV <alekcandr206@gmail.com>
2022-12-01 17:19:04 +0300

    exam preps

---
+++ src/examPreporations/geneticAlgorithm/GeneticAlgorithm.java
@@ -0,0 +1,23 @@
+package examPreporations.geneticAlgorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class GeneticAlgorithm {
+
+    /*private static <Entity> Entity geneticAlgorithm(
+            int entityCount,
+            int numberOfRounds,
+            Supplier<Entity> createEntity
+    ) {
+        List<Entity> entities = new ArrayList<>();
+        for (int i = 0; i < entityCount; i++) {
+            entities.add(createEntity.get());
+        }
+
+        for (int i = 0; i < numberOfRounds; i++) {
+            
+        }
+    }*/
+}
